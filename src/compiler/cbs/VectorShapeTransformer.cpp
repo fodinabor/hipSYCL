421ce50adacb806ca9eeb015cb52b838e636d467
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2022-07-13 13:35:29 +0200

    [CBS] Improve guarding against typed ptr removal.

--- src/compiler/cbs/VectorShapeTransformer.cpp
+++ src/compiler/cbs/VectorShapeTransformer.cpp
@@ -23,10 +23,13 @@ using namespace llvm;
 
 #if LLVM_VERSION_MAJOR < 13
 #define IS_OPAQUE(pointer) constexpr(false)
+#define HAS_TYPED_PTR 1
 #elif LLVM_VERSION_MAJOR < 16
 #define IS_OPAQUE(pointer) (pointer->isOpaquePointerTy())
+#define HAS_TYPED_PTR 1
 #else
 #define IS_OPAQUE(pointer) constexpr(true)
+#define HAS_TYPED_PTR 0
 #endif
 
 hipsycl::compiler::VectorShape GenericTransfer(hipsycl::compiler::VectorShape a) {
@@ -58,11 +61,13 @@ static Type *getElementType(Type *Ty) {
   if (auto VecTy = dyn_cast<VectorType>(Ty)) {
     return VecTy->getElementType();
   }
+#if HAS_TYPED_PTR
   if (auto PtrTy = dyn_cast<PointerType>(Ty)) {
     if IS_OPAQUE (PtrTy)
       return nullptr;
     return PtrTy->getPointerElementType();
   }
+#endif
   if (auto ArrTy = dyn_cast<ArrayType>(Ty)) {
     return ArrTy->getElementType();
   }
@@ -488,7 +493,9 @@ bool returnsVoidPtr(const Instruction &inst) {
   if IS_OPAQUE (inst.getType())
     return true;
 
+#if HAS_TYPED_PTR // otherwise return true from above holds.
   return inst.getType()->getPointerElementType()->isIntegerTy(8);
+#endif
 }
 
 VectorShape VectorShapeTransformer::computeShapeForCastInst(const CastInst &castI) const {
@@ -510,6 +517,7 @@ VectorShape VectorShapeTransformer::computeShapeForCastInst(const CastInst &cast
     if IS_OPAQUE (DestType)
       return VectorShape::strided(castOpStride, 1);
 
+#if HAS_TYPED_PTR
     Type *DestPointsTo = DestType->getPointerElementType();
 
     // FIXME: void pointers are char pointers (i8*), but what
@@ -523,6 +531,7 @@ VectorShape VectorShapeTransformer::computeShapeForCastInst(const CastInst &cast
       return VectorShape::varying();
 
     return VectorShape::strided(castOpStride / typeSize, 1);
+#endif
   }
 
   case Instruction::PtrToInt: {
@@ -530,11 +539,13 @@ VectorShape VectorShapeTransformer::computeShapeForCastInst(const CastInst &cast
     if IS_OPAQUE (SrcType)
       return VectorShape::strided(castOpStride, aligned);
 
+#if HAS_TYPED_PTR
     Type *SrcElemType = SrcType->getPointerElementType();
 
     unsigned typeSize = (unsigned)layout.getTypeStoreSize(SrcElemType);
 
     return VectorShape::strided(typeSize * castOpStride, aligned);
+#endif
   }
 
     // Truncation reinterprets the stride modulo the target type width
